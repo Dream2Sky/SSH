718ccc76db89932e9e7937ceec35ca5872ae74bc
Dream2Sky <1175746962@qq.com>
2017-10-13 17:37:58 +0800

    first commit

---
+++ src/com/dream2sky/action/Account.java
@@ -0,0 +1,15 @@
+package com.dream2sky.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+@SuppressWarnings("serial")
+public class Account extends ActionSupport {
+	public String Login()
+	{
+		return SUCCESS;
+	}
+	public String Logout()
+	{
+		return SUCCESS;
+	}
+}
